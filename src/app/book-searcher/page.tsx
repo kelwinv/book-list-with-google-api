8daaec24e9f07b208950782a8a692f96747535df
kelwinv <kelwin08@gmail.com>
2023-08-01 20:06:41 -0300

    feat(init project)

---
+++ src/app/book-searcher/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React, { useState } from "react";
+
+const SearchBook: React.FC = () => {
+  return (
+    <div>
+      <h1>SearchBook</h1>
+    </div>
+  );
+};
+
+export default SearchBook;
